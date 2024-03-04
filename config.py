027c2ea64f3ccaeef6a4623cd25fef0696b366f1
Forbiddenmain2024 <162131819+Forbidden2025@users.noreply.github.com>
2024-03-04 20:46:23 +0630

    Update config.py
    Signed-off-by: Forbiddenmain2024 <162131819+Forbidden2025@users.noreply.github.com>

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Zenaku10/AnonXMusic",
+    "https://github.com/Forbidden2025/ZenakuXnMusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
