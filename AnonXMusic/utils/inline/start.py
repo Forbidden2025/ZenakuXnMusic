709c9554432f46ce3ec9d154aebd7b1c03afa6a3
Forbiddenmain2024 <162131819+Forbidden2025@users.noreply.github.com>
2024-03-06 23:57:00 +0630

    Update start.py
    Signed-off-by: Forbiddenmain2024 <162131819+Forbidden2025@users.noreply.github.com>

--- AnonXMusic/utils/inline/start.py
+++ AnonXMusic/utils/inline/start.py
@@ -31,7 +31,7 @@ def private_panel(_):
         ],
         [
             InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
-            InlineKeyboardButton(text=_["S_B_7"], url="https://t.me/ForBidden_NoCoUnTeR/2"
+            InlineKeyboardButton(text=_["S_B_7"], url="https://t.me/FoRBIDDen_FeeLinG5/4"
         ),
         ],
     ]
